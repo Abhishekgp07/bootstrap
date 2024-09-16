3629e6c1e5cc03fddc1f81a696b370e4dca39300
abhis <abhishekabhii287@gmail.com>
2024-09-16 17:47:49 +0530

    initial commit

---
+++ Learning_JDSA/src/main/java/org/bootstrap/Learning_Concpets/MultiThreadingIndependent.java
@@ -0,0 +1,62 @@
+package org.bootstrap.Learning_Concpets;
+
+import java.util.Scanner;
+class Msword extends Thread {
+    Msword(String name) {
+        super(name);
+    }
+
+    public void run() {
+        if (getName().equals("Typing")) {
+            Typing();
+        } else if (getName().equals("Chceking")) {
+            spellChecking();
+        } else {
+            Autosave();
+        }
+    }
+
+    void Typing() {
+        try {
+            for (int i = 0; i <= 5; i++) {
+                System.out.println("Typing");
+                Thread.sleep(2000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    void spellChecking() {
+        try {
+            for (int i = 0; i <= 5; i++) {
+                System.out.println("The spelling  Checking");
+                Thread.sleep(3000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    void Autosave() {
+        try {
+            for (int i = 0; i <= 5; i++) {
+                System.out.println("Saving");
+                Thread.sleep(2000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+    class MultiThreadingIndependent{
+        public static void main(String[] args) {
+            Msword m1 = new Msword("Typing");
+            Msword m2 = new Msword("Chceking");
+            Msword m3 = new Msword("Saving");
+            m1.start();
+            m2.start();
+            m3.start();
+        }
+    }
+
