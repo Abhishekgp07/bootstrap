f15aab3620fadfcfcdce4bdaa5d690fc7ccd9f48
abhis <abhishekabhii287@gmail.com>
2024-10-26 22:46:23 +0530

    initial commit

--- Learning_JDSA/src/main/java/org/bootstrap/ArrayPrograms/ArrayMain.java
+++ Learning_JDSA/src/main/java/org/bootstrap/ArrayPrograms/ArrayMain.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
+//O(n)+O(n)+O(n)+O(n)=O(n) for Delete
 class ArrayOperation {
     int n;
     int[] arr;
 
     ArrayOperation(int n) {
         this.n = n;
-        arr = new int[n]; // Initialize the array here
+        arr = new int[n];
     }
 
     public void insertArray() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter " + n + " elements:");
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt(); // Insert elements into the array
+            arr[i] = sc.nextInt();
         }
     }
 
@@ -24,16 +25,91 @@ class ArrayOperation {
         }
         System.out.println();
     }
-}
 
-public class ArrayMain {
-    public static void main(String[] args) {
+    void updateArray(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the array:");
-        int n = sc.nextInt();
+        System.out.println("Enter the element to update in the array:");
+        int pos = sc.nextInt();
+        System.out.println("Enter the  new element");
+        int ele = sc.nextInt();
+        pos=-1;
+        for(int i=0;i<n;i++){
+            if(arr[i]==ele){
+                pos=i;
+                break;
+            }
+        }
+        if(pos>-1){
+            arr[pos]=ele;
+            System.out.println("The element is updated"+ele);
+        }else{
+            System.out.println("The element not found");
+        }
+    }
 
-        ArrayOperation ao = new ArrayOperation(n);
-        ao.insertArray();
-        ao.displayArray();
+    public void DeleteArray() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the element to be deleted:");
+        int ele = sc.nextInt();
+        int pos = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == ele) {
+                pos = i;
+                break;
+            }
+        }
+        if (pos > -1) {
+            int res[] = new int[n-1];
+            int k = 0;
+            for (int i = 0; i < n - 1; i++) {
+                if (i == pos) {
+                    continue;
+                }
+                res[k] = arr[i];
+                k++;
+            }
+            for (int i = 0; i < n - 1; i++) {
+                System.out.println(res[i] + " ");
+            }
+        } else {
+            System.out.println("Element not found:");
+        }
+    }
+
+
+
+    void searchArray(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the element to be searched");
+        int ele = sc.nextInt();
+        int pos=-1;
+                for(int i=0;i<n;i++){
+                    if(arr[i]==ele){
+                        pos=i;
+                        return;
+                    }
+                }
+                if(pos>-1){
+                    System.out.println("Element found in the position"+pos);
+                }else{
+                    System.out.println("element not found");
+                }
     }
 }
+
+    public class ArrayMain {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the size of the array:");
+            int n = sc.nextInt();
+
+            ArrayOperation ao = new ArrayOperation(n);
+            ao.insertArray();
+            ao.displayArray();
+            ao.updateArray();
+            ao.searchArray();
+            ao.DeleteArray();
+
+        }
+    }
+
